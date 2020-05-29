769e22407a0948b2015a42bc4c68eeed787df8ff
Ngọ Anh Đức <lanhhuyet510@users.noreply.github.com>
2020-05-29 09:30:08 +0700

    version 1

---
+++ dice45_io.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3.7
+
+import json
+import requests
+import secrets
+import os
+
+TOTAL_PROFIT = 0
+TOTAL_BET = 0
+TARGET_PROFIT = 20
+CURRENT_BALANCE = 0
+DOMAIN = "io"
+HEADERS = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0',
+            'Accept': 'text/html, */*; q=0.01',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Cookie': '__cfduid=de8a85b217d72de202589ae686fb1ff081589452975; SESSION=Y1ZLRitNSnptbkpkVW1JcStWSWpNZHJxUXhIbTdUTFhiaTdESllMRDdGMEh5T2h0SG5OS2crOUpLcSs5N1BHOEM4bnBMUXZHNUlEeEhlaG41ZXVqR2ZtNDY2NzU4Q08yeEhRMVNMb1orOXlrK3FoczcrRE5DM0Y5UCtja28yR2FmcFc1Y29nQkxZdHhPcTRnSU5uWVJCNHVKRDUxdDE0aUg5RG9odit0M3JSZXMxOVhhQi81OVg1WVJ1Y3dxVndTMlVKbHRuM2VMcEV3dGhZUDF6Q1pndysyc3FvcWs0U1BYNnBJU0dBVk5YYm1ZOG5XK2Y4ZFBpbVVxNXZXWk9OTW92Rm1yVGVjcjJ5OExETjVlTDdINTNhSTBUSWlRdVFmNENzN3ZwNUdoQXNZbWMxV0lGU1VZalZpU28zaVpZZDd4UmVEemx4T1BrS0VrTWg5N0xtaGIvNFFEVGoxVG9SREdCV3Z4M2FXb3VoMHBFL1dXVVU1YnVOWnZmYitWMkVuQjZuQUw4QktQYktmckRsa2c3M0JnQlowMTduNzlNOFhuL1YzajJYYjRnSWxDdGI1MGRGdmtBZXlOaHZtY1NXeU9nSnNLZkdzQ09jb2hEUnp4ZWs3MVpyZ1dTQVpRcTFzNEJJWk5kUFVQcmswb05jWVI3eThseHIvdmM4RUxZNDg%3D; uROOM=en; intercom-id-vvzhqtge=c29e1280-ea69-4524-bf4b-e8aa75755fce; intercom-session-vvzhqtge='
+        }
+serverSeedHash = ""
+
+
+def randomSeed():
+    global serverSeedHash
+    global BALANCE
+    global HEADERS
+    try:
+        API_ENDPOINT = "https://luckygames." + DOMAIN + "/request/"
+        DATA = {'action':'randomizeSeed', 'hash' : '1d056abee9c79faee5e34ef6977b2510468ee513', 'previousSeedHash':''}
+        r = requests.post(url = API_ENDPOINT, headers = HEADERS, data = DATA, timeout = 10)
+        serverSeedHash = json.loads(r.text).get('serverSeedHash','null')
+    except:
+        serverSeedHash = randomSeed()
+    return serverSeedHash
+
+def writeHistory(betID, clientSeed, _prevServerSeed, serverSeedHash, resultNumber, profit):
+    LOG_FILE = "game_history.txt"
+    logWriter = open(LOG_FILE, "a+")
+    logWriter.write("%s|%s|%s|%s|%s|%s \r\n" % (betID, clientSeed, _prevServerSeed, serverSeedHash, resultNumber, profit))
+    logWriter.close()
+
+def writeHighRoll(betID, clientSeed, serverSeedHash, resultNumber, betAmount, profit):
+    global DOMAIN
+    logWriter = open('highBets_' + DOMAIN +'.txt', "a+")
+    logWriter.write("%s|%s|%s|%s|%s|%s \r\n" % (betID, clientSeed, serverSeedHash, resultNumber, betAmount, profit))
+    logWriter.close()
+
+def doBet(coinName, betAmount, prediction, direction):
+    global TOTAL_BET
+    global TOTAL_PROFIT
+    global HEADERS
+    global CURRENT_BALANCE
+    global serverSeedHash
+    try:
+        clientSeed = secrets.token_hex(16)
+        if TOTAL_BET == 0:
+            serverSeedHash = randomSeed()
+        API_ENDPOINT = "https://luckygames." + DOMAIN + "/play/"
+        DATA = {'game':'dice', 'coin' : coinName, 'betAmount': betAmount, 'prediction':prediction, 'direction':direction,'clientSeed':clientSeed, 'serverSeedHash': serverSeedHash, 'hash':'1d056abee9c79faee5e34ef6977b2510468ee513'}
+        r = requests.post(url = API_ENDPOINT, headers = HEADERS, data = DATA, timeout = 10)
+        r_response = json.loads(r.text)
+        _resultNumber = r_response.get('resultNumber','null')
+        _gameResult = r_response.get('gameResult','null')
+        _result = r_response.get('result','null')
+        _profit = r_response.get('profit','null')
+        _betID = r_response.get('id','null')
+        _prevServerSeedHash = r_response.get('prevServerSeedHash','null')
+        _prevServerSeed = r_response.get('prevServerSeed','null')
+        _resultNumber = r_response.get('resultNumber','null')
+        serverSeedHash = r_response.get('serverSeedHash', 'null')
+        CURRENT_BALANCE = float(r_response.get('balance', '0'))
+        
+        if _result == 'false' or _result == False or _result == 'null':
+            #print ("Trying again..")
+            serverSeedHash = randomSeed()
+            doBet(coinName, betAmount, prediction, direction)
+        else:
+            if float(betAmount) >= 1:
+                writeHighRoll(_betID, clientSeed, _prevServerSeedHash, _resultNumber, betAmount, _profit)
+            #writeHistory(_betID, clientSeed, _prevServerSeedHash, _resultNumber, _profit)
+            #print ("Bet Amount: " + betAmount)
+            #print ("Game Result: " + _gameResult)
+            #print ("Profit: " + _profit)
+            TOTAL_PROFIT = TOTAL_PROFIT + float(_profit)
+            #print ("TOTAL PROFIT: {0:.10f}".format(TOTAL_PROFIT))
+            return _gameResult
+    except:
+        #print ("Trying again..")
+        serverSeedHash = randomSeed()
+        doBet(coinName, betAmount, prediction, direction)
+
+def main():
+    global TOTAL_PROFIT
+    global CURRENT_BALANCE
+    global TARGET_PROFIT
+    global TOTAL_BET
+    coinName = 'doge'
+    baseBetAmount = 0.0000025000
+    betAmount = 0.0000025000
+    direction = 'over'
+    prediction = 55
+    while TOTAL_PROFIT < TARGET_PROFIT:
+        #secretsGenerator = secrets.SystemRandom()
+        #choseRandomDirection = secretsGenerator.randint(0,99)
+        #choseRandomDirection = 60
+        #if choseRandomDirection < 50:
+        #    direction = 'under'
+        #    prediction = 45
+        #else:
+        #    direction = 'over'
+        #    prediction = 55
+        _gameResult = doBet(coinName, '{0:10f}'.format(betAmount), prediction, direction)
+        TOTAL_BET = TOTAL_BET + 1
+        if _gameResult == "win":
+            if TOTAL_BET > 5000:
+                break
+            if CURRENT_BALANCE > 1:
+                baseBetAmount = CURRENT_BALANCE / 268435455
+            betAmount = baseBetAmount
+        else:
+            betAmount = betAmount * 2
+    logWriter = open('completed_' + DOMAIN + '.txt', "a+")
+    logWriter.write("Completed! Profit: " + str(TOTAL_PROFIT) + " \r\n")
+    logWriter.close()
+            
+
+if __name__== "__main__" :
+    main()
